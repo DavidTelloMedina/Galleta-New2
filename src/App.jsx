9a3b34d8c0e32ceb5dc91b8e76fb032fa4d9541d
David Tello <82037546+DavidTelloMedina@users.noreply.github.com>
2023-07-04 10:15:15 -0500

    fixroutes

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import cookie from './components/data/phrases.json'
 
 function App() {
   const back = ['/fondosImg/fondo1.jpg','/fondosImg/fondo2.jpg','/fondosImg/fondo3.jpg','/fondosImg/fondo4.jpg']
-  const [index, useIndex] = useState (0)
+  const [index, setIndex] = useState (0)
   const [bg, usebg]= useState (0)
 
   const lucky = () => {
+    setIndex(Math.floor(Math.random()* cookie.length)) 
     if (bg < back.length -1) {
       usebg(bg + 1)
     }else{
       usebg(0)
     }     
-    
   }  
-  useIndex(Math.floor(Math.random()* cookie.length))       
+      
+
 
   document.body.style= `background-image: url(${back[bg]})`
   return (
